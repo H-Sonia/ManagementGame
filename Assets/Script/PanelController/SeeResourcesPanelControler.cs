76d0f3afaf837c3502273ab0c62a00aed16f1927
H-Sonia <sonia.hammami@ecole.ensicaen.fr>
2022-04-28 16:12:14 +0100

    quit button

--- Assets/Script/PanelController/SeeResourcesPanelControler.cs
+++ Assets/Script/PanelController/SeeResourcesPanelControler.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class SeeResourcesPanelControler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject SeeResourcesPanel;
+    public void Quit()
     {
-        
+        SeeResourcesPanel.SetActive(false);
     }
 }
