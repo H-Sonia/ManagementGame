c7b64262745327c2de1ec1a5a9bd5e362ac6be22
H-Sonia <sonia.hammami@ecole.ensicaen.fr>
2022-05-06 15:16:13 +0100

    adding audio in the menu and the game

---
+++ Assets/Script/Audio/GameAudio.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudio : MonoBehaviour
+{
+    public AudioClip MapMusic;
+    public AudioClip DormMusic;
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource.clip = MapMusic;
+        audioSource.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    public void PlayDormMusic()
+    {
+        audioSource.clip = DormMusic;
+        audioSource.Play();
+    }
+
+    public void PlayMapMusic()
+    {
+        audioSource.clip = MapMusic;
+        audioSource.Play();
+    }
+}
