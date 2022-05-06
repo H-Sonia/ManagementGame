afbb786ae7ca6b1a2ca139f23b093e3a5504b953
H-Sonia <sonia.hammami@ecole.ensicaen.fr>
2022-05-06 11:28:01 +0100

    update inventory to randomize characters

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -7,11 +7,14 @@ public class Inventory : MonoBehaviour
 {
     public List<FoodData> content = new List<FoodData>();
     public int currentResource = 0;
+    public CharactersData empty;
     public CharactersData[] Characters;
     public List<CharactersData> DeceasedCharacters = new List<CharactersData>();
     public List<CharactersData> Newcomers = new List<CharactersData>(); 
     public int currentCharacter;
+    public UIDisplay ui;
     public static Inventory instance;
+    
 
     private void Awake()
     {
@@ -22,33 +25,102 @@ public class Inventory : MonoBehaviour
         }
         instance = this;
     }
-
+    public void Start()
+    {
+        FillCharacters();
+        ui.UpdateMainUi();
+    }
     public void ClearInventory()
     {
         content.Clear();
     }
 
-    // public void FillNewcomers()
-    // {
-    //     for(int i=0; i < CharacterDatabase.instance.allCharacters.Length;i++)
-    //     {
-            
-    //     }
-    // }
 
-    // public void RandomAssignement()
-    // {
-    //     System.Random random = new System.Random();
-    //     int listCount = random.Next(Characters.Length);
-    //     for(i=0; i< listCount; i++)
-    //     {
-    //         System.Random random2 = new System.Random();
-    //         int character = random2.Next(CharacterDatabase.instance.allCharacters.Length);
-    //         Characters[i] = CharacterDatabase.instance.allCharacters[i];
 
-    //     }
+    public void FillNewcomers()
+    {
+        for (int i = 1; i < CharacterDatabase.instance.allCharacters.Length; i++)
+        {
+            Newcomers.Add(CharacterDatabase.instance.allCharacters[i]);
+        }
+    }
 
-    // }
+    public void FillCharacters()
+    {
+        FillNewcomers();
+        List<CharactersData> emptyList = new List<CharactersData>();
+        for (int i=0; i < 12; i++)
+        {
+            emptyList.Add(empty);
+        }
+        Characters = emptyList.ToArray();
 
-   
+
+        System.Random random = new System.Random();
+        int charactersNb = random.Next(12);
+        for (int i = 0; i < charactersNb; i++)
+        {
+            int index = random.Next(Newcomers.Count);
+            Characters[i] = Newcomers[index];
+            Newcomers.Remove(Newcomers[index]);
+        }
+    }
+
+
+    void SomeoneDisappears()
+    {
+        System.Random random = new System.Random();
+        int index = random.Next(12);
+        if (Characters[index].id != 0)
+        {
+           DeceasedCharacters.Add(Characters[index]);
+           Characters[index] = empty;
+        }
+    }
+
+    int IsEmptySpot()
+    {
+        for (int i = 0; i < Characters.Length; i++)
+        {
+            if (Characters[i].id == 0)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void SomeoneAppears()
+    {
+        int index = IsEmptySpot();
+
+        if (Newcomers.Count <= 0)
+        {
+            return;
+        }
+
+        if (index != -1)
+        {
+            Characters[index] = Inventory.instance.Newcomers[0];
+            Newcomers.Remove(Inventory.instance.Newcomers[0]);
+        }
+    }
+
+    public void UpdateCharactersLists()
+    {
+        System.Random random = new System.Random();
+        int probability = random.Next(100);
+        if(probability > 70)
+        {
+            SomeoneDisappears();
+        }
+        System.Random random2 = new System.Random();
+        int probability2 = random2.Next(100);
+        if (probability2 > 70)
+        {
+            SomeoneAppears();
+        }
+        ui.UpdateMainUi();
+    }
+
 }
--- Assets/Script/UIControls/UIDisplay.cs
+++ Assets/Script/UIControls/UIDisplay.cs
@@ -13,37 +13,14 @@ public class UIDisplay : MonoBehaviour
     public GameObject[] idButtons;
     public CharactersData empty;
 
-    void Start()
-    {
-                UpdateCheck();
-    }
-
-    public void UpdateCheck()
-    {
-        System.Random random = new System.Random();
-        int probability = random.Next(100);
-        if(probability > 70)
-        {
-            SomeoneDisappears();
-        }
-        System.Random random2 = new System.Random();
-        int probability2 = random2.Next(100);
-        if (probability2 > 70)
-        {
-            SomeoneAppears();
-        }
-        UpdateMainUi();  
-    }
     
     public void UpdateMainUi()
     {
-
-        // Random number for the length of the list and then random numbers for the characters used
         for (int i = 0; i < names.Length; i++)
         {
             CharactersData character = Inventory.instance.Characters[i];
 
-
+            
             if (character.id == 0)
             {
 
@@ -55,58 +32,12 @@ public class UIDisplay : MonoBehaviour
             else
             {
                 names[i].text = character.firstname + " " + character.surname;
+                giveButtons[i].SetActive(true);
+                seeButtons[i].SetActive(true);
+                idButtons[i].SetActive(true);
             }
         }
     }
 
-    void SomeoneDisappears()
-    {
-        System.Random random = new System.Random();
-        int index = random.Next(12);
-        if (Inventory.instance.Characters[index].id != 0)
-        {
-            Inventory.instance.DeceasedCharacters.Add(Inventory.instance.Characters[index]);
-            Inventory.instance.Characters[index] = empty;
-        }
-    }
-
-    int IsEmptySpot()
-    {
-        for(int i=0; i < Inventory.instance.Characters.Length;i++)
-        {
-            if(Inventory.instance.Characters[i].id == 0)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    void SomeoneAppears()
-    {
-        int index = IsEmptySpot();
-        Debug.LogWarning("in someoneAppears");
-        Debug.LogWarning(Inventory.instance.Newcomers.Count);
-
-        if (Inventory.instance.Newcomers.Count <= 0)
-        {
-            Debug.LogWarning("new commers vide");
-            return;
-        }
-
-        if(index != -1)
-        {
-            Debug.LogWarning("Pas vide");
-            Inventory.instance.Characters[index] = Inventory.instance.Newcomers[0];
-            Inventory.instance.Newcomers.Remove(Inventory.instance.Newcomers[0]);
-            //Debug.LogWarning(Inventory.instance.Newcomers.Count);
-            Debug.LogWarning("Here");
-            for (int i = 0; i < Inventory.instance.Newcomers.Count; i++)
-            {
-                Debug.LogWarning(Inventory.instance.Newcomers[i].firstname);
-            }
-            Debug.LogWarning("Loop end");
-            //MainUi.UpdateMainUi();
-        }
-    }
+    
 }
