a9fe65be396c3ca74a62475a922647981eddb212
H-Sonia <sonia.hammami@ecole.ensicaen.fr>
2022-06-01 15:49:58 +0100

    change in audio system

--- Assets/Script/Audio/GameAudio.cs
+++ Assets/Script/Audio/GameAudio.cs
@@ -7,9 +7,20 @@ public class GameAudio : MonoBehaviour
     public AudioClip MapMusic;
     public AudioClip DormMusic;
     public AudioClip KitchenMusic;
+    public AudioClip ArenaMusic;
     public AudioSource audioSource;
+    // 0 = map, 1 = dorm, 2 = kitchen, 3 = arena
+    int room;
+
+    [SerializeField]
+    GameObject dorm, boxing, kitchen;
+
     void Start()
     {
+        dorm = GameObject.Find("Dorm");
+        boxing = GameObject.Find("Boxing");
+        kitchen = GameObject.Find("Kitchen");
+
         audioSource.clip = MapMusic;
         audioSource.Play();
 
@@ -18,11 +29,67 @@ public class GameAudio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!audioSource.isPlaying)
+        int whichRoom = inWhichRoom();
+        if (room != whichRoom)
         {
-            audioSource.Play();
+            room = whichRoom;
+            ChangeMusic(room);
+        }
+        else
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        
+    }
+
+    public int inWhichRoom()
+    {
+        if(dorm.activeSelf)
+        {
+            return 1;
+        }
+        if(kitchen.activeSelf)
+        {
+            return 2;
+        }
+        if(boxing.activeSelf)
+        {
+            return 3;
+        }
+        return 0;
+    }
+
+    public void ChangeMusic(int whichRoom)
+    {
+        switch(whichRoom)
+        {
+            case 0:
+                audioSource.clip = DormMusic;
+                audioSource.Play();
+                break;
+            case 1:
+                audioSource.clip = MapMusic;
+                audioSource.Play();
+                break;
+            case 2:
+                audioSource.clip = KitchenMusic;
+                audioSource.Play();
+                break;
+            case 3:
+                audioSource.clip = ArenaMusic;
+                audioSource.Play();
+                break;
+            default:
+                Debug.LogWarning("Room index out of range");
+                break;
         }
     }
+
+
+
 
     public void PlayDormMusic()
     {
@@ -41,4 +108,5 @@ public class GameAudio : MonoBehaviour
         audioSource.clip = KitchenMusic;
         audioSource.Play();
     }
+
 }
