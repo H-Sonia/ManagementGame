46f516ba2981fed8c2bce70188f532d6a6a053a8
H-Sonia <sonia.hammami@ecole.ensicaen.fr>
2022-08-11 13:00:50 +0200

    Add credit and change warning

--- Assets/Script/UIControls/MainMenu/MainMenu.cs
+++ Assets/Script/UIControls/MainMenu/MainMenu.cs
@@ -29,7 +29,7 @@ public class MainMenu : MonoBehaviour
 
   public void openCreditPanel()
   {
-    creditPanel.SetActive(true);
+    SceneManager.LoadScene("Credits");
   }
   public void closeCreditPanel()
   {
