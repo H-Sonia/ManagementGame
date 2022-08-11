79374829ac14f0c100edb290377262a627c7d39d
H-Sonia <sonia.hammami@ecole.ensicaen.fr>
2022-08-11 16:05:11 +0200

    continue/new game button

--- Assets/Script/UIControls/MainMenu/MainMenu.cs
+++ Assets/Script/UIControls/MainMenu/MainMenu.cs
@@ -7,7 +7,35 @@ public class MainMenu : MonoBehaviour
 {
   public GameObject creditPanel;
   public GameObject settingPanel;
-    public GameObject NewGamePanel;
+  public GameObject NewGamePanel;
+  public GameObject NewGameButton; 
+  public GameObject ContinueButton; 
+
+  void Start()
+  {
+    string filePathDataChar = Application.persistentDataPath + "/CharactersData.json";
+    string filePathDataSave = Application.persistentDataPath + "/DataSave.json";
+    if(System.IO.File.Exists(filePathDataChar))
+    {
+      Debug.Log(filePathDataChar);
+    }
+
+    if(System.IO.File.Exists(filePathDataChar))
+        {
+          Debug.Log("Active continue button");
+          NewGameButton.SetActive(false);
+          ContinueButton.SetActive(true);
+        }
+      else
+      {
+        Debug.Log("Active new game button");
+        NewGameButton.SetActive(true);
+        ContinueButton.SetActive(false);
+      }
+
+  }
+
+
   
   public void LoadGame()
   {
@@ -16,7 +44,18 @@ public class MainMenu : MonoBehaviour
 
   public void NewGame()
   {
-        NewGamePanel.SetActive(true);
+        string filePath = Application.persistentDataPath + "/CharactersData.json";
+        if(System.IO.File.Exists(filePath))
+        {
+            System.IO.File.Delete(filePath);
+        }
+        filePath = Application.persistentDataPath + "/DataSave.json";
+        if (System.IO.File.Exists(filePath))
+        {
+            System.IO.File.Delete(filePath);
+        }
+
+        SceneManager.LoadScene("IntroScreen");
   }
   public void QuitGame()
   {
